af846e11b763c080895b65a52f375f38ab4f1bcb
Amelie Ruefenacht <aruefenach@student.ethz.ch>
2025-05-08 00:56:20 +0200

    Still working on tests

--- docs.md
+++ docs.md
@@ -1,10 +1,14 @@
-npm install --save react-router-dom  in frontend
-npm install --save-dev hardhat  in pokemon
+npm install --save react-router-dom -> in frontend
+npm install --save-dev hardhat -> in pokemon
 nvm install 20.18.0 / nvm use 20.18.0 if already installed
 npx hardhat compile
 npx hardhat node
 new Terminal:
-npx hardhat ignition deploy ignition/modules/AuctionModule.ts --network localhost 
-	Optionally with -reset
+npx hardhat ignition deploy ignition/modules/AuctionModule.ts --network localhost --reset
 npx hardhat run --network localhost scripts/setPokemonImageURIs.ts
 in frontend: npm start
+
+For testing:
+install:
+ npm install -D @openzeppelin/test-helpers
+ npm install --save-dev @nomiclabs/hardhat-truffle5 @nomiclabs/hardhat-web3 'web3@^1.0.0-beta.36'
--- test/AuctionManager.ts
+++ test/AuctionManager.ts
@@ -1,7 +1,41 @@
-import {
-    time,
-    loadFixture,
-  } from "@nomicfoundation/hardhat-toolbox/network-helpers";
-  import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
-  import { expect } from "chai";
-  import hre from "hardhat";
+// import {
+//   time,
+//   loadFixture,
+// } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+// import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+// import { expect } from "chai";
+// import hre from "hardhat";
+
+// describe("Auction", function () {
+//   // We define a fixture to reuse the same setup in every test.
+//   // We use loadFixture to run this setup once, snapshot that state,
+//   // and reset Hardhat Network to that snapshot in every test.
+//   async function deployPokemonCards() {
+//     // Contracts are deployed using the first signer/account by default
+//     const [owner, otherAccount] = await hre.ethers.getSigners();
+
+//     const Pokemon = await hre.ethers.getContractFactory("PokemonTest");
+//     const pokemon = await Pokemon.deploy();
+
+//     const Auction = await hre.ethers.getContractFactory("AuctionManager");
+//     const auction = await Auction.deploy(pokemon.getAddress());
+
+//     return { pokemon, auction, owner, otherAccount };
+//   }
+
+//   describe("Create", function () {
+
+//     it("Should change ownership pokemon card", async function () {
+//       const { pokemon, auction, owner } = await loadFixture(deployPokemonCards);
+//       const biddingTime = 1;
+//       const startingBid = 1;
+//       const pokemonId = await pokemon.mint(owner);
+
+//       await auction.create(1, 1, pokemonId);
+      
+//       expect(await pokemon.isOwner(owner, pokemonId));
+//     });
+
+//   });
+
+// });
