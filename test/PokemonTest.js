15cda32f673c96e87a359e053cc612b7213f22a9
Amelie Ruefenacht <aruefenach@student.ethz.ch>
2025-05-08 00:58:11 +0200

    Pokemon test: changing from ts file to js

--- test/PokemonTest.js
+++ test/PokemonTest.js
@@ -1,10 +1,5 @@
-import {
-  time,
-  loadFixture,
-} from "@nomicfoundation/hardhat-toolbox/network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
-import { expect } from "chai";
-import hre from "hardhat";
+require("@nomiclabs/hardhat-ethers");
+const { expect } = require("chai");
 
 describe("PokemonCards", function () {
   // We define a fixture to reuse the same setup in every test.
